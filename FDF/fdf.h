ea8669f4d43dc7a0a310ed1b29cccf52c5b7fa44
Jae Min Jo <jaejo@c3r6s4.42gyeongsan.kr>
2025-01-13 19:30:19 +0900

    fdf

---
+++ FDF/fdf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaejo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 18:53:57 by jaejo             #+#    #+#             */
+/*   Updated: 2025/01/13 18:54:00 by jaejo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include "./minilibx/mlx.h"
+#include "./ft_printf/ft_printf.h"
+
+# endif
