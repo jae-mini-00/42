c31a0452e5ee56c2a386092574bf0262bf8ea6db
Jae Min Jo <jaejo@c3r6s4.42gyeongsan.kr>
2025-01-13 19:30:19 +0900

    fdf

---
+++ FDF/ft_printf/libft/ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaejo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/03 17:09:47 by jaejo             #+#    #+#             */
+/*   Updated: 2024/10/03 17:38:32 by jaejo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*temp;
+	size_t	i;
+
+	i = 0;
+	temp = (char *)malloc (sizeof(char) * ft_strlen(s) + 1);
+	if (temp == 0)
+		return (NULL);
+	while (s[i])
+	{
+		temp[i] = s[i];
+		i++;
+	}
+	temp[i] = '\0';
+	return (temp);
+}
