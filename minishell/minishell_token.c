7e8d3cc7c5a8d5cb643cf83d094c47360ebc3b77
Jae Min Jo <jaejo@c2r5s4.42gyeongsan.kr>
2025-03-14 15:14:49 +0900

    0314 c

--- minishell/minishell_token.c
+++ minishell/minishell_token.c
@@ -39,60 +39,18 @@ void add_token(t_token **head, char *value, t_type type)
 		temp->next = new;
     }
 }
-/*
-int	quote_check(char *str)
-{
-	int	i;
-	int	flag;
-	int	flag2;
-
-	i = 0;
-	flag = 0;
-	flag2 = 0;
-	while (str[i])
-	{
-		if (str[i] == '"')
-			flag++;
-		if (str[i] == '\'')
-			flag2++;
-		i++;
-	}
-	return (flag);
-}
-
-char	**minishell_split(char *str)
-{
-	int		i;
-	char	**split;
-	char	*temp;
-
-	i = 0;
-	split = ft_split(str, ' ');
-	while (split[i])
-	{
-		if (ft_strncmp(split[i], "|", 2) == 0)
-			i++;
-		else if (ft_strncmp(split[i], ">", 2) == 0 || ft_strncmp(split[i], "<", 2) == 0)
-			i++;
-		else if (ft_strncmp(split[i], ">>", 3) == 0 || ft_strncmp(split[i], "<<", 3) == 0)
-			i++;
-		else if (ft_strncmp(split[i], "$", 2) == 0)
-			i++;
-		else if (ft_strchr(split[i], '"') || ft_strchr(split[i], '\''))
-	}
-}
 
 t_token	*make_token(char *str)
 {
 	int		i;
-	t_token	*token;
 	char	**data;
+	t_token	*token;
 
 	i = 0;
 	data = minishell_split(str);
 	while (data[i])
 	{
-		if (ft_strncmp(data[i], "|", 2) == 0)
+		if (ft_strncmp(data[i], "|") == 0)
 			add_token(&token, data[i], PIPE);
 		else if (ft_strncmp(data[i], ">", 2) == 0 || ft_strncmp(data[i], "<", 2) == 0)
 			add_token(&token, data[i], REDIRECTION);
