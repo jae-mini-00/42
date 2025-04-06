cd3e633556f6757faf32e65e0f9b196c6cb96357
jaejo <gottkfwoals@naver.com>
2025-04-06 19:11:47 +0900

    here_doc 1

---
+++ minishell/run/minishell_here_doc.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_here_doc.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaejo <jaejo@student.42gyeongsan.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/04 20:48:41 by jaejo             #+#    #+#             */
+/*   Updated: 2025/04/06 18:46:23 by jaejo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell_run.h"
+
+static char	*fd_name(int count)
+{
+	char	*name;
+
+	name = ft_strjoin("temp", ft_itoa(count));
+	return (name);
+}
+static void	here_doc_trance(t_token *token, char *name)
+{
+	token->type = REDIRECTION;
+	free(token->value);
+	token->value = ft_strdup("<");
+	free(token->next->value);
+	token->next->value = name; 
+}
+static void	minishell_here_doc(t_token *token, char *name)
+{
+	char	*temp;
+	char	*end;
+	int		fd;
+	int		len;
+
+	end = ft_strjoin(token->next->value, "\n");
+	len = ft_strlen(end);
+	fd = open(name, O_WRONLY | O_CREAT | O_TRUNC, 6044);
+	while (1)
+	{
+		write(1, "> ", 2);
+		temp = get_next_line(0);
+		if (ft_strncmp(temp, end, len) == 0)
+		{
+			free(temp);
+			free(end);
+			here_doc_trance(token, name);
+			close(fd);
+			return ;
+		}
+		write(fd, temp, ft_strlen(temp));
+		free(temp);
+	}
+}
+void	minishell_here_doc_check(t_data *minishell)
+{
+	t_token *now;
+	
+	now = minishell->token;
+	while (now)
+	{
+		if (now->type == HERE_DOC)
+		{
+			minishell->here_doc_count++;
+			minishell_here_doc(now, fd_name(minishell->here_doc_count));
+			now = now->next;
+		}
+		else 
+			now = now->next;
+	}
+}
