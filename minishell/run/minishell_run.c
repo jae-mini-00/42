4669e2efe3d389db5bc75cfaf8ef40aa34854386
jaejo <gottkfwoals@naver.com>
2025-04-19 19:50:30 +0900

    신택스 에러 처리

--- minishell/run/minishell_run.c
+++ minishell/run/minishell_run.c
@@ -3,22 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_run.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaejo < jaejo@student.42gyeongsan.kr>      +#+  +:+       +#+        */
+/*   By: jaejo <jaejo@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 18:26:38 by jaejo             #+#    #+#             */
-/*   Updated: 2025/04/17 23:04:50 by jaejo            ###   ########.fr       */
+/*   Updated: 2025/04/19 19:50:10 by jaejo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell_run.h"
 
-static int	check_pipe(t_data *minishell)
+static int	check_pipe(t_token *data)
 {
 	t_token	*temp;
 	int		flag;
 
 	flag = 0;
-	temp = minishell->token;
+	temp = data;
 	while (temp)
 	{
 		if (temp->type == PIPE && temp->next)
@@ -30,24 +30,53 @@ static int	check_pipe(t_data *minishell)
 	return (flag);
 }
 
+static int	syntax_err_check(t_token *data)
+{
+	t_token *token;
+
+	token = data;
+	while (token)
+	{
+		if (token->type == REDIRECTION || token->type == HERE_DOC)
+		{
+			if (!token->next || (token->next && (token->next->type == REDIRECTION || \
+				token->next->type == HERE_DOC || token->next->type == PIPE)))
+			{
+				printf("syntax error near unexpected token newline\n");
+				return (false);
+			}
+		}
+		token = token->next;
+	}
+	if (check_pipe(data) == -1)
+	{
+		printf("syntax error near unexpected token newline\n");
+		return (false);
+	}
+	return (true);
+}
+
 void	minishell_run(t_data *minishell)
 {
 	int	check;
 	int	status;
 
 	status = 0;
-	minishell_here_doc_check(minishell);
-	check = check_pipe(minishell);
-	if (check == -1)
-		return ;
-	if (check)
-		multi_fork(minishell, check + 1, 0);
-	else if (builtin_type_check(minishell->token))
-		builtin_check(minishell);
+	if (syntax_err_check(minishell->token))
+	{
+		minishell_here_doc_check(minishell);
+		check = check_pipe(minishell->token);
+		if (check)
+			multi_fork(minishell, check + 1, 0);
+		else if (builtin_type_check(minishell->token))
+			builtin_check(minishell);
+		else
+			solo_fork(minishell);
+		token_fd_close(minishell->token, 1);
+		if (minishell->pid != 0)
+			waitpid(minishell->pid, &status, 0);
+		minishell->exit_code = (status >> 8) & 0xFF;;
+	}
 	else
-		solo_fork(minishell);
-	token_fd_close(minishell->token, 1);
-	if (minishell->pid != 0)
-		waitpid(minishell->pid, &status, 0);
-	minishell->exit_code = status;
+		minishell->exit_code = 2;
 }
