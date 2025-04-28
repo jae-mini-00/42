5c45ebf82159e8e038e70b0b77eb3310c478b7b7
Jae Min Jo <jaejo@c3r14s6.42gyeongsan.kr>
2025-04-29 06:36:53 +0900

    end

--- minishell/builtin/minishell_export_unset.c
+++ minishell/builtin/minishell_export_unset.c
@@ -6,7 +6,7 @@
 /*   By: jaejo <jaejo@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 07:07:20 by jaejo             #+#    #+#             */
-/*   Updated: 2025/04/29 05:49:24 by jaejo            ###   ########.fr       */
+/*   Updated: 2025/04/29 06:34:27 by jaejo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,24 @@ static char	*get_env_name(char *env)
 	int		len;
 	char	*env_name;
 
-	i = 0;
+	i = -1;
 	len = env_len(env);
-	if (env[len] != '=')
+	if (len == -1)
+	{
+		write (2, "export:'", 9);
+		write (2, env, ft_strlen(env));
+		write (2, "': not a valid identifier\n", 27);
+		return (NULL);
+	}
+	if ((env[len] != '=' && env[len] != '+') || \
+		(env[len] == '+' && env[len + 1] != '='))
 		return (NULL);
 	env_name = (char *)malloc(sizeof(char) * (len + 2));
 	if (!env_name)
 		return (NULL);
-	while (i <= len)
-	{
+	while (++i < len)
 		env_name[i] = env[i];
-		i++;
-	}
+	env_name[i++] = '=';
 	env_name[i] = '\0';
 	return (env_name);
 }
@@ -50,22 +56,26 @@ static void	export_run(t_data *minishell, char **cmd, int i)
 {
 	char	*env_name;
 	int		len;
+	int		j;
 
-	env_name = get_env_name(cmd[1]);
+	j = -1;
+	env_name = get_env_name(cmd[i]);
 	if (!env_name)
 	{
 		minishell->exit_code = 1;
 		return ;
 	}
 	len = ft_strlen(env_name);
-	while (minishell->env[++i])
-		if (ft_strncmp(minishell->env[i], env_name, len) == 0)
+	while (minishell->env[++j])
+		if (ft_strncmp(minishell->env[j], env_name, len) == 0)
 			break ;
-	if (minishell->env[i])
-		change_env(minishell, cmd[1], i);
+	if (minishell->env[j])
+		change_env(minishell, cmd[i], j);
 	else
-		make_env(minishell, cmd[1]);
+		make_env(minishell, cmd[i]);
 	free(env_name);
+	if (minishell->exit_code != 1)
+		minishell->exit_code = 0;
 }
 
 void	ft_export(t_data *minishell, char **cmd, t_token *start)
@@ -78,9 +88,12 @@ void	ft_export(t_data *minishell, char **cmd, t_token *start)
 	else
 		io_dup (start, 0, 1, 0);
 	export_printf(minishell->env, cmd[1]);
-	if (cmd[1])
+	i = 1;
+	minishell->exit_code = 0;
+	while (cmd[i])
 	{
 		export_run(minishell, cmd, i);
+		i++;
 	}
 }
 
@@ -108,4 +121,5 @@ void	ft_unset(t_data *minishell, char **cmd, t_token *start)
 	if (minishell->env[i])
 		remove_minishell_env(minishell, env_name, len);
 	free(env_name);
+	minishell->exit_code = 0;
 }
