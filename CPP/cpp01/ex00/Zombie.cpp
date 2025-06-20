dc1cbb4d96cc0b77cda8e5403257c052d5018f3f
Jae Min Jo <jaejo@c3r11s4.42gyeongsan.kr>
2025-06-20 12:29:07 +0900

    cpp01 start

---
+++ CPP/cpp01/ex00/Zombie.cpp
@@ -0,0 +1,21 @@
+#include "./Zombie.hpp"
+
+Zombie::Zombie(std::string name)
+{
+    this->name = name;
+    std::cout   << "Zombie " << name << " create!" << std::endl;
+}
+
+Zombie::~Zombie()
+{
+    std::cout   << "Zombie " << name
+                << " destroyed!"
+                << std::endl;
+}
+
+void Zombie::announce( void )
+{
+    std::cout   << name << ": "
+                << "BraiiiiiiinnnzzzZ..."
+                << std::endl;
+}
