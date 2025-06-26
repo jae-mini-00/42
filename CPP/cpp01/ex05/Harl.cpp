99cd1f9f532bf7891cf7c016f9bfff470acbc75c
Jae Min Jo <jaejo@c3r12s2.42gyeongsan.kr>
2025-06-26 18:36:31 +0900

    cpp01 start 6/26 ex05 start

---
+++ CPP/cpp01/ex05/Harl.cpp
@@ -0,0 +1,36 @@
+#include "Harl.hpp"
+
+Harl::Harl(std::string level)
+{
+    this->level = level;
+}
+
+Harl::~Harl()
+{
+
+}
+
+void    Harl::debug(void)
+{
+
+}
+
+void    Harl::info(void)
+{
+
+}
+
+void    Harl::warning(void)
+{
+
+}
+
+void    Harl::error(void)
+{
+
+}
+
+void    complain(std::string level)
+{
+
+}
