1f8da509cc3868a7f36df98b63e9324016224ddc
Jae Min Jo <jaejo@c3r11s2.42gyeongsan.kr>
2025-06-27 16:32:03 +0900

    cpp01 start 6/26 ex06 start

--- CPP/cpp01/ex05/Harl.cpp
+++ CPP/cpp01/ex05/Harl.cpp
@@ -1,36 +1,60 @@
 #include "Harl.hpp"
 
-Harl::Harl(std::string level)
+Harl::Harl()
 {
-    this->level = level;
+    ;
 }
 
 Harl::~Harl()
 {
-
+    ;
 }
 
 void    Harl::debug(void)
 {
-
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout   << "Fetching user data from API with user_id=12345"
+                << std::endl;
 }
 
 void    Harl::info(void)
 {
-
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout   << "User successfully logged in. user_id=12345"
+                << std::endl;
 }
 
 void    Harl::warning(void)
 {
-
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout   << "User profile image not found, using default avatar. user_id=12345"
+                << std::endl;
 }
 
 void    Harl::error(void)
 {
-
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout   << "Failed to save user data to database. user_id=12345, error=ConnectionTimeout"
+                << std::endl;
 }
 
-void    complain(std::string level)
+void    Harl::complain(std::string level)
 {
-
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    void(Harl::*functions[4])(void) = 
+    {
+        &Harl::debug,
+        &Harl::info,
+        &Harl::warning,
+        &Harl::error
+    };
+    for(int i = 0; i < 4; i++)
+    {
+        if(levels[i] == level)
+        {
+            (this->*functions[i])();
+            return ;
+        }
+    }
+    std::cout   << "Level not find" << std::endl;
 }
