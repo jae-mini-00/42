d873f85ae81bab2b8995c988f5ecd8cc0cf9e90d
Jae Min Jo <jaejo@c1r12s4.42gyeongsan.kr>
2025-09-16 10:10:10 +0900

    CPP09 ex02 end?

--- cpp/cpp09/ex02/main.cpp
+++ cpp/cpp09/ex02/main.cpp
@@ -7,17 +7,28 @@ int main(int ac, char **av)
 		std::cerr	<< "Error: invalid argument" << std::endl;
 		return (1);
 	}
-
 	PmergeMe sort;
-
 	if (!sort.vec_format(ac, av))
 	{
 		std::cerr	<< "Error: invalid argument" << std::endl;
 		return (1);
 	}
-	std::cout	<< "Before: ";
-	sort.print();
-	sort.fordJohnsonSortVector();
-
+	double vec_elapsed;
+	double deque_elapsed;
+	{	std::cout	<< "Before: ";
+		sort.vec_print();
+		clock_t start = clock();
+		sort.fordJohnsonSortVector();
+		clock_t end = clock();
+		vec_elapsed = static_cast<double>(end - start);
+	}
+	{
+		clock_t start = clock();
+		sort.fordJohnsonSortDeque();
+		clock_t end = clock();
+		deque_elapsed = static_cast<double>(end - start);
+	}
+	sort.is_sort();
+	sort.timer_print(vec_elapsed, deque_elapsed);
 	return (0);
 }
